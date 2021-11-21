37285497cbba0c8b7d0ff9acf74c6d171d3dbe7c
huycongtran1120 <huycongtran01@gmail.com>
2021-11-21 20:40:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/smartrt/MainMenu.java
@@ -0,0 +1,27 @@
+package com.example.smartrt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainMenu extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        //hook
+        drawerLayout=findViewById(R.id.draw_layout);
+
+
+
+    }
+}
